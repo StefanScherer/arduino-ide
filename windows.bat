344c22fb108261e302726ce030deb3e90822776a
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-09 23:01:44 +0100

    started windows host install script

---
+++ windows.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Ensuring .NET 4.0 is installed
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/StefanScherer/arduino-ide/install/InstallNet4.ps1'))"
+echo Installing Chocolatey
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+where cinst
+if ERRORLEVEL 1 goto set_chocolatey
+goto inst
+:set_chocolatey
+set ChocolateyInstall=%SystemDrive%\Chocolatey
+set PATH=%PATH%;%ChocolateyInstall%\bin
+:inst
+
+call cinst virtualbox
+call cinst vagrant
+cd /D %USERPROFILE%\Documents
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "((new-object net.webclient).DownloadFile('https://raw.github.com/StefanScherer/arduino-ide/master/Vagrantfile', 'Vagrantfile'))"
+call vagrant up
